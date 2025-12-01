443ce953b2cb3d140e61cd1b29bfb50e6236e4f1
Khusanboy <khusanboygoyibberdiev@gmail.com>
2025-12-01 01:26:10 -0600

    Fix duplicate mysql declaration

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // =============================
 
 const express = require("express");
-const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
